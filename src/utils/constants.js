c9fde25efa85d3a1d80d51197581401f31fc370d
1nthe4byss <christian4onos@outlook.com>
2025-12-07 15:05:11 +0100

    7th Dec: button and login

---
+++ src/utils/constants.js
@@ -0,0 +1,49 @@
+export const NIGERIAN_UNIVERSITIES = [
+    'University of Lagos (UNILAG)',
+    'University of Ibadan (UI)',
+    'Obafemi Awolowo University (OAU)',
+    'University of Nigeria, Nsukka (UNN)',
+    'Ahmadu Bello University (ABU)',
+    'University of Benin (UNIBEN)',
+    'University of Ilorin (UNILORIN)',
+    'Lagos State University (LASU)',
+    'Covenant University',
+    'Babcock University',
+    'Federal University of Technology, Akure (FUTA)',
+    'Federal University of Technology, Minna (FUTMINNA)',
+    'Nnamdi Azikiwe University (UNIZIK)',
+    'Rivers State University',
+    'University of Port Harcourt (UNIPORT)',
+    'Other'
+];
+
+export const DEPARTMENTS = [
+    'Computer Science',
+    'Engineering',
+    'Medicine',
+    'Law',
+    'Business Administration',
+    'Economics',
+    'Mass Communication',
+    'Accounting',
+    'Architecture',
+    'Pharmacy',
+    'Nursing',
+    'Mathematics',
+    'Physics',
+    'Chemistry',
+    'Biology',
+    'English',
+    'Political Science',
+    'Psychology',
+    'Other'
+];
+
+export const ACADEMIC_LEVELS = [
+    '100 Level',
+    '200 Level',
+    '300 Level',
+    '400 Level',
+    '500 Level',
+    '600 Level'
+];
